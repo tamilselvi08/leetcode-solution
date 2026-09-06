4f1a0d66a8a5297995de876d2151e9a484dd7395
Tamilselvi.S <tamilselvism08@gmail.com>
2026-09-06 11:32:46 +0530

    Time: 2 ms (48.04%), Space: 46.9 MB (19.73%) - LeetHub

--- 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
+++ 3069-distribute-elements-into-two-arrays-i/3069-distribute-elements-into-two-arrays-i.java
@@ -1,29 +1,26 @@
 class Solution {
-    public int[] resultArray(int[] nums) {
-        ArrayList<Integer> arr1 = new ArrayList<>();
-        ArrayList<Integer> arr2 = new ArrayList<>();
 
+    public int[] resultArray(int[] nums) {
+        int n = nums.length;
+        List<Integer> arr1 = new ArrayList<>();
+        List<Integer> arr2 = new ArrayList<>();
         arr1.add(nums[0]);
         arr2.add(nums[1]);
-
-        for(int i = 2; i < nums.length; i++){
-            int val1 = arr1.get(arr1.size() - 1);
-            int val2 = arr2.get(arr2.size() - 1);
-
-            if(val1 > val2) arr1.add(nums[i]);
-            else arr2.add(nums[i]);
+        for (int i = 2; i < n; i++) {
+            if (arr1.get(arr1.size() - 1) > arr2.get(arr2.size() - 1)) {
+                arr1.add(nums[i]);
+            } else {
+                arr2.add(nums[i]);
+            }
         }
-
-        int m = arr1.size() + arr2.size();
-        int[] arr = new int[m];
+        int[] res = new int[n];
         int idx = 0;
-
-        for(int i = 0; i < arr1.size(); i++)
-            arr[idx++] = arr1.get(i);
-
-        for(int i = 0; i < arr2.size(); i++)
-            arr[idx++] = arr2.get(i);
-
-        return arr;
+        for (int x : arr1) {
+            res[idx++] = x;
+        }
+        for (int x : arr2) {
+            res[idx++] = x;
+        }
+        return res;
     }
 }
